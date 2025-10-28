e378d8fa12d8254589e1ba34aa986fd92dad1080
subencheng <ben.su@conductorone.com>
2025-10-27 17:49:24 -0700

    linter

--- cmd/baton-dropbox/main.go
+++ cmd/baton-dropbox/main.go
@@ -2,12 +2,9 @@ package main
 
 import (
 	"context"
-	"fmt"
-	"log"
 
 	cfg "github.com/conductorone/baton-dropbox/pkg/config"
 	"github.com/conductorone/baton-dropbox/pkg/connector"
-	"github.com/conductorone/baton-dropbox/pkg/connector/dropbox"
 	"github.com/conductorone/baton-sdk/pkg/config"
 	"github.com/conductorone/baton-sdk/pkg/connectorrunner"
 )
@@ -17,35 +14,4 @@ var version = "dev"
 func main() {
 	ctx := context.Background()
 	config.RunConnector(ctx, "baton-dropbox", version, cfg.ConfigurationSchema, connector.NewLambdaConnector, connectorrunner.WithSessionStoreEnabled())
-}
-
-func configure(ctx context.Context, dropboxCfg *cfg.Dropbox) error {
-	appKey := dropboxCfg.AppKey
-	if dropboxCfg.AppKey == "" {
-		return fmt.Errorf("app key is required")
-	}
-
-	appSecret := dropboxCfg.AppSecret
-	if dropboxCfg.AppSecret == "" {
-		return fmt.Errorf("app secret is required")
-	}
-
-	client, err := dropbox.NewClient(ctx, dropbox.Config{
-		AppKey:    appKey,
-		AppSecret: appSecret,
-	})
-	if err != nil {
-		return err
-	}
-	code, err := client.Authorize(ctx, appKey, appSecret)
-	if err != nil {
-		return err
-	}
-
-	_, _, refreshToken, err := client.RequestAccessToken(ctx, code)
-	if err != nil {
-		return err
-	}
-	log.Printf("\nrefresh token: %s\n", refreshToken)
-	return nil
 }
